d7748d96cbba395d7b8329cdc9b2815d75f23867
zhenyulincs <a1349636970@gmail.com>
2024-06-25 21:37:10 +0000

    Add api url for backend

--- frontend/pages/ai_chat.tsx
+++ frontend/pages/ai_chat.tsx
@@ -116,7 +116,7 @@ const AI_Chat: NextPage = () => {
               placeholder='Input disabled, Please use the suggest prompt first...'
               
             />
-            <button disabled type="submit" className='chatbot-send-button flex rounded-md items-center justify-center px-2.5 origin:px-3'>
+            <button type="submit" className='chatbot-send-button flex rounded-md items-center justify-center px-2.5 origin:px-3'>
               <svg width="20" height="20" viewBox="0 0 20 20">
                 <path d="M2.925 5.025L9.18333 7.70833L2.91667 6.875L2.925 5.025ZM9.175 12.2917L2.91667 14.975V13.125L9.175 12.2917ZM1.25833 2.5L1.25 8.33333L13.75 10L1.25 11.6667L1.25833 17.5L18.75 10L1.25833 2.5Z" />
               </svg>
