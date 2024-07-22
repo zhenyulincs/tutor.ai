8b28428cbd907f01a03023690589931d613e76bd
jonnald <143930617+jonnald0430@users.noreply.github.com>
2024-07-22 01:26:11 -0700

    Cleaning and finishing UI #1 (#31)
    * category each compones by pages
    
    * Add hyper link for second page
    
    * Add simple chatbot page
    
    * Add more suggest prompt
    
    * add preset questions to the ai chat page
    
    * fix typescript variable type issue
    
    * Add export status page config
    
    * Add api url for backend
    
    * add gitignore on backend
    
    * Updated GitHub logo link to own tutor.ai repository (https://github.com/zhenyulincs/tutor.ai) -Jonnald
    
    * Changed GitHub button link to own repository (https://github.com/zhenyulincs/tutor.ai)
    
    * Edited hero section of site, replaced "Get Started" button text with "Try tutor.ai" and updated subheading
    
    * added features 1, 2, and 3 (incomplete), edited grammar on hero section and ai_chat.tsx, edited sizing slightly on navbar, added a sample image from ai bot
    
    * Added features and example use cases to site (incomplete)
    
    * Edited hero section of page, updated subheading
    
    * added sample image of chatbot
    
    * Edited chatbot grammar
    
    * Update CI workflow
    
    * reformatted features2 section, &apos; for apostrophes instead of char
    
    * split CI to merge-deployment and pr build check
    
    * completed features section, implemented technologies section with images, created faq, deleted plans section
    
    * &apos; formatting faq fix
    
    * space
    
    * another space
    
    * reformatted apostrophe &apos; on faq
    
    * synced navbar theme to page theme and dividing sections of features, made ids and anchored to each section
    
    * added bordering and padding to features dropdown and removed extra placeholders, changed feature names, changed website hook, added some names to link directory
    
    * category each compones by pages
    
    * Add simple chatbot page
    
    * add preset questions to the ai chat page
    
    * Add api url for backend
    
    * completed features section, implemented technologies section with images, created faq, deleted plans section
    
    * merge to development
    
    * fixed react/no-unescaped-entities issue
    
    * fixed react/no-unescaped-entities
    
    * remove technologies import
    
    * adjusted navbar and tutor.ai icon, edited features page (will add appropriate text and formatting for feature 2), removed feature numbering, simplified footer, adjusted fontsize of second header section, added workflow icon
    
    * merge from LLM
    
    * merge to LLM
    
    * add &apos;
    
    * add image to frontend
    
    * fix &apos;`
    
    * remove extra link in footer
    
    * add icon to ai_chat
    
    * Current final flowchart of feature 1 implemented, rough draft flowchart for feature 2 and feature 3 implemented, footer directory removed, footer copyright added
    
    * Project prototype done
    
    ---------
    
    Co-authored-by: zhenyulincs <a1349636970@gmail.com>

--- backend/backend.py
+++ backend/backend.py
@@ -4,6 +4,7 @@ import os
 import config  # This will load the appropriate .env file
 import sys
 from flask_cors import CORS, cross_origin
+from classes_vector_storage_map import get_vector_storage
 sys.path.append(os.path.join(os.path.dirname(os.getcwd())))
 from LLM.rag import rag
 # Initialize Flask app
@@ -14,7 +15,8 @@ CORS(app)
 client = OpenAI(
     # Defaults to os.environ.get("OPENAI_API_KEY")
 )
-rag = rag()
+rag_bot = rag()
+
 
 @app.route('/api/ai', methods=['POST'])
 @cross_origin(origins="*", methods=["GET", "POST"], allow_headers=["Content-Type"])
@@ -22,27 +24,43 @@ def ai_endpoint():
     data = request.json
     if not data or 'messages' not in data:
         return jsonify({'error': 'Invalid input'}), 400
-
     user_input = data['messages'][-1]["content"]
-    print(user_input)
+    histroy = data['messages'][0:-1]
+    
     try:
-        ai_response = rag.chat(user_input)
-        print(ai_response)
-        # # Call the OpenAI API with the user's input
-        # response = client.chat.completions.create(
-        #     model="gpt-3.5-turbo",  # Use "gpt-3.5-turbo" or the appropriate engine
-        #     messages=[
-        #         {"role": "system", "content": "You are a helpful assistant."},
-        #         *user_input
-        #         ],
-        #     max_tokens=150  # Adjust max tokens as needed
-        # )
-        # ai_response = response.choices[0].message.content.strip()
-        # # Return the response
-        return ai_response, 200
+
+        functionality = data['messages'][3]["content"]
+        classes = data['messages'][1]["content"]
+        rag_bot.convert_to_chat_message(
+            [
+                {"role": "user", "content": "You are a helpful assistant named WisdomWhiz."},
+                *histroy
+            ]
+        )
+        if len(data['messages']) <= 6:
+            rag_bot.set_vector_storage(path=get_vector_storage(classes))
+            sufix = f"The user want to {functionality}, can you ask user some necessary question first"
+            user_input += sufix
+            response = rag_bot.chat_regular(user_input)
+            
+        else:
+            sufix = f"Answer only base on given textbook: C Programming modern apporch. If user ask questions not relative to C programming, please remind the user"
+            user_input += sufix
+            if "Class preview/review" in functionality :
+                response = rag_bot.get_youtube_type_response(query=user_input)
+            elif "Help with Homework" in functionality:
+                user_input += "Don't direct give answer in any cases. Guide the user step by step. use the knowledge from textbook"
+                response = rag_bot.chat_rag(query=user_input)
+            else:
+                response = rag_bot.chat_rag(query=user_input)
+        rag_bot.clear_chat_history()
+
+        return response, 200
 
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        print(e)
+        return f"Error: Please Try again", 500
+
 
 if __name__ == '__main__':
     app.run(port=int(os.getenv('PORT', 5000)), debug=os.getenv('DEBUG', 'False') == 'True',use_reloader=True)
